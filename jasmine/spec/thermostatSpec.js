913eccb7d994cbdf0a558f54fa6838de13f0dc07
Peter Miklos <petermiklos@Peters-MacBook-Pro.local>
2016-09-13 12:24:03 +0100

    start temperature added

--- jasmine/spec/thermostatSpec.js
+++ jasmine/spec/thermostatSpec.js
@@ -1,1 +1,16 @@
 'use strict';
+
+describe("Thermostat", function() {
+  var thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  })
+
+  describe("check temperature", function() {
+    it("has 20 degrees at start", function() {
+      expect(thermostat.checkTemperature()).toEqual(20);      
+    })
+  })
+
+});
--- jasmine/src/thermostat.js
+++ jasmine/src/thermostat.js
@@ -1,5 +1,12 @@
 "use strict";
 
 function Thermostat() {
-
+  const _START_TEMPERATURE = 20;
+  this.currentTemperature = _START_TEMPERATURE;
 };
+
+Thermostat.prototype = {
+  checkTemperature: function() {
+    return this.currentTemperature;
+  }
+}
