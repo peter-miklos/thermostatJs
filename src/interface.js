acaeb038fdafe2143a7486f0ef65cf3e0c260a87
Peter Miklos <petermiklos@Peters-MacBook-Pro.local>
2016-09-14 13:00:29 +0100

    ajax, basic styling added

--- src/interface.js
+++ src/interface.js
@@ -29,9 +29,25 @@ $(document).ready(function() {
     updateTemperature();
   })
 
+  $("#weather-submit").click(function() {
+    var city = $("#weather-city").val();
+    displayWeather(city);
+  });
+
   function updateTemperature() {
     $('#temperature').text(thermostat.checkTemperature());
     $("#temperature").attr("class", thermostat.checkEnergyUsage());
     $("#power-saving-status").text(thermostat.getPowerSavingModeStatus());
   }
+
+  function displayWeather(city) {
+    var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city;
+    var token = "&mode=html&APPID=1bed5f3ef484a3d22e5d5d4f5c7ae34d";
+    var units = "&units=metric"
+    $.get(url+token+units, function(data) {
+      $("#weather-app")[0].innerHTML = data;
+    })
+  }
+
+
 });
