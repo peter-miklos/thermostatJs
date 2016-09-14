a0f58a53beecc41171750c246d5abd2369061d1d
Lawrence Dawson <lawrencedawson@hotmail.co.uk>
2016-09-14 19:02:32 +0100

    added jquery and used bootstrap css

--- src/Thermostat.js
+++ src/Thermostat.js
@@ -23,6 +23,7 @@ Thermostat.prototype.decreaseTemp = function () {
   else {
     this._currentTemp -= 1;
   }
+};
 
 Thermostat.prototype.switchPowerMode = function () {
   if (this._powerSaving) {
@@ -50,7 +51,57 @@ Thermostat.prototype.indicator = function () {
   }
 };
 
-};
+//first we check the document has loaded & display with business logic.
+$( document ).ready(function() {
+    thermostat = new Thermostat();
+    $( "#temp" ).html(thermostat._currentTemp);
+    $( ".power_saving" ).html("Power Saving: " + thermostat._powerSaving);
+    if (thermostat.indicator() === "green" ) {
+      $( "#temp" ).css("color", "green");
+    }
+    else if (thermostat.indicator() === "red") {
+      $( "#temp" ).css("color", "red" );
+    }
+    else {
+      $( "#temp" ).css("color", "yellow" );
+    }
+
+//this listens to clicking the increase button.
+  $( '[name="increase_btn"]' ).click(function(){
+      thermostat.increaseTemp();
+    });
+
+    $( '[name="decrease_btn"]' ).click(function(){
+      thermostat.decreaseTemp();
+    });
+
+    $( '[name="reset_btn"]' ).click(function(){
+      thermostat.reset();
+    });
+
+    $( '[name="power_mode_btn"]' ).click(function(){
+      thermostat.switchPowerMode();
+    });
+
+//this listens to every click on a page.
+    $( document ).click(function(){
+        $( "#temp" ).html(thermostat._currentTemp);
+        $( ".power_saving" ).html("Power Saving: " + thermostat._powerSaving);
+
+        if (thermostat.indicator() === "green" ) {
+          $( "#temp" ).css("color", "green");
+        }
+        else if (thermostat.indicator() === "red") {
+          $( "#temp" ).css("color", "red" );
+        }
+        else {
+          $( "#temp" ).css("color", "yellow" );
+        }
 
 
-console.log('HI');
+    });
+
+
+
+
+});
