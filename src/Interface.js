69d01d1b2c69fdf042a842768a5a3d143e6d8d9d
Peter Miklos <petermiklos@Peters-MacBook-Pro.local>
2016-09-15 15:16:45 +0100

    some formatting added

--- src/Interface.js
+++ src/Interface.js
@@ -1,17 +1,18 @@
 //first we check the document has loaded & display with business logic.
 $( document ).ready(function() {
-    thermostat = new Thermostat();
-    $( "#temp" ).html(thermostat._currentTemp);
-    $( ".power_saving" ).html("Power Saving: " + thermostat._powerSaving);
-    if (thermostat.indicator() === "green" ) {
-      $( "#temp" ).css("color", "green");
-    }
-    else if (thermostat.indicator() === "red") {
-      $( "#temp" ).css("color", "red" );
-    }
-    else {
-      $( "#temp" ).css("color", "yellow" );
-    }
+  displayWeather("Worcester");
+  thermostat = new Thermostat();
+  $( "#temp" ).html(thermostat._currentTemp);
+  $( ".power_saving" ).html("Power Saving: " + thermostat._powerSaving);
+  if (thermostat.indicator() === "green" ) {
+    $( "#temp" ).css("color", "green");
+  }
+  else if (thermostat.indicator() === "red") {
+    $( "#temp" ).css("color", "red" );
+  }
+  else {
+    $( "#temp" ).css("color", "yellow" );
+  }
 
 //this listens to clicking the increase button.
   $( '[name="increase_btn"]' ).click(function(){
@@ -53,17 +54,12 @@ $( document ).ready(function() {
         }
     });
 
-    function displayWeather(city, country){
-      var url = "http://api.wunderground.com/api/85e43c1814d737e7/conditions/q/";
-
-      $.get(url + country + "/" + city + ".json", function(data){
-    
-        $("#weather_icon").text(data.current_observation.icon_url);
-        $("#full_location").text(data.current_observation.display_location.full);
-        $("#temp_c").text(data.current_observation.temp_c);
-        $("#wind_mph").text(data.current_observation.wind_mph);
-        $("#feelslike_c").text(data.current_observation.feelslike_c);
-
+    function displayWeather(city){
+      var url = "http://api.openweathermap.org/data/2.5/weather?q=" + city;
+      var token = "&mode=html&APPID=1bed5f3ef484a3d22e5d5d4f5c7ae34d";
+      var units = "&units=metric"
+      $.get(url + token + units, function(data){
+        $("#weather")[0].innerHTML = data;
         });
       }
   });
